b68c4b841ee3af2acf008c6e2e5b4dc71cd1ee72
Samuel Vitek <Samuel.Vitek@aimtecglobal.com>
2022-10-31 03:29:39 +0100

    Draw rectangles.

--- controller/controller.js
+++ controller/controller.js
@@ -1,5 +1,15 @@
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
 
-ctx.fillStyle = "green";
-ctx.fillRect(10, 10, 150, 100);
+let rectangles = [];
+
+ctx.fillStyle = 'green';
+ctx.shadowBlur = 0;
+
+//Row 1
+for (let i = 0; i < 8; i++) {
+    for (let j = 0; j < 8; j++) {
+        ctx.fillRect(30 + (j * 30), 15 + (i * 15), 25, 12.5);
+    }
+}
+

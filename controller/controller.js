9eaa047626458824a6ddc1fdd3cdc6bbc49c1327
Samuel Vitek <Samuel.Vitek@aimtecglobal.com>
2022-10-31 04:09:56 +0100

    Quick change (for local testing) because of screen resolution scale

--- controller/controller.js
+++ controller/controller.js
@@ -1,6 +1,3 @@
-// window.onload = alert('Please view the image of my screen in the repository.\n' +
-//     'I have recommended resolution 125% and I don\'t trust it to be the same on different pc.');
-
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
@@ -12,7 +9,9 @@ ctx.shadowBlur = 0;
 //Row 1
 for (let i = 0; i < 8; i++) {
     for (let j = 0; j < 8; j++) {
-        ctx.fillRect(30 + (j * 30), 15 + (i * 15), 25, 12.5);
+        ctx.fillRect(30 + (j * 30), 15 + (i * 15), 25, 12.5); //For sreen resolution scale 125%
+
+        //ctx.fillRect(30 + (j * 17.5), 15 + (i * 15), 12.5, 12.5); //For sreen resolution scale 100%
     }
 }
 
